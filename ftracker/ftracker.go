87cce43f2d510a9f6c5a2c7b5e20e1251f57f8c4
Your Name <you@example.com>
2023-11-06 21:23:34 +0400

    idea folder added to gitignore, code comments removed

--- ftracker/ftracker.go
+++ ftracker/ftracker.go
@@ -26,7 +26,6 @@ func meanSpeed(action int, duration float64) float64 {
 }
 
 func ShowTrainingInfo(action int, trainingType string, duration, weight, height float64, lengthPool, countPool int) string {
-	// ваш код здесь
 	switch {
 	case trainingType == "Бег":
 		distance := distance(action)
@@ -35,6 +34,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч."+
 			"\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
 			trainingType, duration, distance, speed, calories)
+
 	case trainingType == "Ходьба":
 		distance := distance(action)
 		speed := meanSpeed(action, duration)
@@ -42,6 +42,7 @@ func ShowTrainingInfo(action int, trainingType string, duration, weight, height 
 		return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч."+
 			"\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
 			trainingType, duration, distance, speed, calories)
+
 	case trainingType == "Плавание":
 		distance := distance(action)
 		speed := swimmingMeanSpeed(lengthPool, countPool, duration)
